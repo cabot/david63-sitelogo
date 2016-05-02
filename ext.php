27f122e7e7a8fad3c3a9c5730ccf8f6cc090eaee
david63 <david.wood@pear-tree.org.uk>
2016-05-02 13:21:39 +0100

    Re commit

---
+++ ext.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+* @package Site Logo Extension
+* @copyright (c) 2014 david63
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+namespace david63\sitelogo;
+
+class ext extends \phpbb\extension\base
+{
+	const SITE_LOGO_VERSION	= '2.1.0';
+
+	const LOGO_POSITION_LEFT	= 0;
+	const LOGO_POSITION_CENTER	= 1;
+	const LOGO_POSITION_RIGHT	= 2;
+
+	/**
+	* Enable extension if phpBB version requirement is met
+	*
+	* @var string Require 3.2.0-a1 due to updated 3.2 syntax
+	*
+	* @return bool
+	* @access public
+	*/
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+
+		if (!phpbb_version_compare($config['version'], '3.2.0-a1', '>='))
+		{
+			$this->container->get('language')->add_lang('ext_sitelogo', 'david63/sitelogo');
+			trigger_error($this->container->get('language')->lang('VERSION_32') . adm_back_link(append_sid('index.' . $this->container->getParameter('core.php_ext'), 'i=acp_extensions&amp;mode=main')), E_USER_WARNING);
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
---
+++ styles/all/template/css_switch_fn.js
@@ -0,0 +1,67 @@
+;(function($, document) {
+	$(document).ready(function () {
+		var headBgColour	= '';
+		var h1Colour		= '';
+		var pColour			= '';
+		var bgImage			= '';
+
+		// Replicate the style colours and background image in the new site logo css
+    	headBgColour	= $('.headerbar').css('background-color');
+		h1Colour		= $('h1').css('color');
+		pColour			= $('p').css('color');
+		bgImage			= $('.headerbar').css('background-image');
+
+		$('.sl-headerbar').css('background-color', headBgColour);
+		$('#sl-new-desc h1').css('color', h1Colour);
+		$('#sl-new-desc p').css('color', pColour);
+		$('.sl-headerbar').css('background-image', bgImage);
+
+		$('a .site_logo').css({'background-image': siteLogo, 'background-repeat': 'no-repeat', 'background-position': '0% 0%', 'background-size': '100%', 'height': logoHeight, 'width': logoWidth, '-webkit-border-radius': logoCorners, '-moz-border-radius': logoCorners, 'border-radius': logoCorners});
+
+		// Remove the site name and description
+		if (siteNameSupress == true) {
+			$('#site-description h1').css('display', 'none');
+			$('#site-description p').css('display', 'none');
+    	}
+
+		// Place the site logo (and text) in the centre
+		if (siteLogoCentre == true) {
+			$('#site-description').css({'padding-top': '10px', 'text-align': 'center', 'width': '100%'});
+			$('#site-description .logo').css('float', 'none');
+			$('.search-header').css('margin-top', '-40px');
+			$('#sl-new-desc h1').css({'text-align': 'center', 'width': '100%'});
+			$('#sl-new-desc p').css({'text-align': 'center', 'width': '100%'});
+    	}
+
+		// Place the site logo on the right
+		if (siteLogoRight == true) {
+			$('#site-description').css({'text-align': 'left', 'width': '100%', 'margin-left': '10px'});
+			$('#site-description .logo').css({'float': 'right', 'margin-right': '10px'});
+			$('#sl-new-desc h1').css({'clear': 'both', 'float': 'right', 'margin-right': '10px'});
+			$('#sl-new-desc p').css({'clear': 'both', 'float': 'right', 'margin-right': '10px'});
+
+			if (searchBelow != true) {
+				$('.search-header').css({'float': 'left', 'margin-top': '-10px', 'margin-left': '10px'});
+			}
+    	}
+
+		// Move the search box to the top nav bar
+		if (searchBelow == true) {
+			$('.search-header').css({'float': 'right', 'margin-top': '5px', 'box-shadow': 'none'});
+    	}
+
+		// Use the override text colour to replace the default in the header
+		if (useOverrideColour == true) {
+			$('#site-description h1').css('color', '#' + overrideColour);
+			$('#site-description p').css('color', '#' + overrideColour);
+			$('#sl-new-desc h1').css('color', '#' + overrideColour);
+			$('#sl-new-desc p').css('color', '#' + overrideColour);
+    	}
+
+		// Use the Site Logo banner
+		if (useBanner == true) {
+			$('.headerbar').css({'background-image': 'url("' + siteLogoBanner + '")', 'height': bannerHeight + 'px', 'border-radius': borderRadius + 'px', 'display': 'block', 'margin': '0 auto', 'max-width': '100%'});
+    	}
+	})
+
+})(jQuery, document);
